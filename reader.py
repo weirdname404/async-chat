b86d22cdbcce43b7ff221d0b7e1eaae86b60f50f
Alexander Lebedev <lebedev.alex.dev@gmail.com>
2020-12-15 16:13:30 +0300

    refactoring:   - fixed setup text   - added socket close handle   - fixed default args   - added timeout to read func

--- reader.py
+++ reader.py
@@ -6,19 +6,35 @@ from datetime import datetime as dt
 
 logger = logging.getLogger(__name__)
 data_format = "[%d.%m.%y %H:%M]"
+setup_text = """
+Host: {}
+Port: {}
+Outfile: {}
+"""
 
 
-async def listen(host, port, file):
+async def readline(reader):
+    return await reader.readline()
+
+
+async def listen(host, port, file, timeout=5):
     if file is None:
         file = 'out.log'
     reader, writer = await asyncio.open_connection(host, port)
     logger.info(f"STARTS TO LISTEN {host}:{port}")
-    while True:
-        data = await reader.readline()
-        async with aiofiles.open(file, 'a') as f:
-            date = dt.now().strftime(data_format)
-            await f.write(f"{date} {data.decode()}")
-        await asyncio.sleep(0)
+    async with aiofiles.open(file, 'a') as f:
+        try:
+            while True:
+                data = await asyncio.wait_for(readline(reader), timeout=timeout)
+                data = data.decode()
+                logger.info(data)
+                date = dt.now().strftime(data_format)
+                await f.write(f"{date} {data}")
+        except asyncio.exceptions.TimeoutError:
+            logger.info("Connection lost.")
+        finally:
+            writer.close()
+            logger.debug("Reader session closed.")
 
 
 if __name__ == "__main__":
@@ -29,24 +45,24 @@ if __name__ == "__main__":
         datefmt="%H:%M:%S",
     )
     parser = argparse.ArgumentParser()
-    parser.add_argument('--host', help='host listen to')
-    parser.add_argument('--port', help='port')
-    parser.add_argument('--out', help='output file name')
-    args = parser.parse_args()
-
-    # default vars
-    if (host := args.host) is None:
-        host = "minechat.dvmn.org"
-    if (port := args.port) is None:
-        port = 5000
-    if (outfile := args.out) is None:
-        outfile = 'chat.history'
-
-    setup_text = (
-        "Setup",
-        f"Host: {host}",
-        f"Port: {port}",
-        f"Outfile: {outfile}"
+    parser.add_argument(
+        '--host', default="minechat.dvmn.org", help='host listen to'
     )
-    logger.info("\n".join(setup_text))
-    asyncio.run(listen(host, port, outfile))
+    parser.add_argument(
+        '--port', default=5000, type=int, help='port'
+    )
+    parser.add_argument(
+        '--out', default='chat.history', help='output file name'
+    )
+    parser.add_argument(
+        '--debug', action='store_true', help='debug mode'
+    )
+    args = parser.parse_args()
+    # debug mode
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    args = (args.host, args.port, args.out)
+
+    logger.info(setup_text.format(*args))
+    asyncio.run(listen(*args))
